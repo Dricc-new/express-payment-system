f7e277deb217f9bb4521f4773d77bc44756fc368
Dricc-new <diegoramoncc17@gmail.com>
2023-08-01 00:41:45 -0400

    Main layout

--- src/index.js
+++ src/index.js
@@ -1,14 +1,37 @@
-const express = require('express')
-const expbhs = require('express-handlebars')
-const path = require('path')
+import express from 'express'
+import { engine } from 'express-handlebars'
+import path from 'path'
+import { fileURLToPath } from 'url';
 // Inicializacion
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express()
 
+// Middleware
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+// Routes
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    res.render('partials/index')
+})
+
+app.use(router)
+
 // Settings
-app.set('views', path.join(__dirname, 'views'))
-app.engine('.hbs', expbhs({
-    
-}))
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+    defaultLayout: 'main',
+    extname: '.hbs',
+}));
+
+app.set('view engine', '.hbs');
+
+// Static files
+app.use(express.static(path.join(__dirname,'../public')))
 
 // Start Server
 app.listen(3000, () => {
